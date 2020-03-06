6857406ea2ff8c21e1821c6c2bdc45527e3ea1c0
web-prog <61786931+Viccttor@users.noreply.github.com>
2020-03-06 20:04:30 +0600

    Add a new site

---
+++ site2/script.js
@@ -0,0 +1,60 @@
+let width_slider = document.querySelector('.slider-content')
+let pet = document.querySelectorAll('.slider-pets')
+let l_arr = document.querySelector('.l_arrow') 
+let r_arr = document.querySelector('.r_arrow') 
+let right = 0;
+
+// function parallax(e){
+// 	document.querySelector('.header-content').style.transform = `translateX(${event.clientX/50}px) translateY(${event.clientY/50}px)`
+// 	document.querySelector('.header-logo').style.transform = 'none'
+// }
+
+function scrollTo(e,toWhat){
+	e.preventDefault()
+	target = document.querySelector(toWhat)
+	console.log(target)
+	target.scrollIntoView({block: "center", behavior: "smooth"})
+}
+
+const sliderLeft = () => {
+	if (right === 0) {
+		right = 660
+	}
+	else right = right - 330
+	width_slider.style.right = right + 'px';
+}
+
+const sliderRight = () => {
+	if (right === 660) {
+		right = 0
+	}
+	else right = right + 330
+	width_slider.style.right = right + 'px';
+}
+l_arr.addEventListener('click', sliderLeft)
+r_arr.addEventListener('click', sliderRight)
+for (var i = 0; i < pet.length; i++) {
+	pet[i].addEventListener('mousemove', function(){
+	this.querySelector('a').style.background = '#f1cdb3'
+})
+	pet[i].addEventListener('mouseout', function(){
+	this.querySelector('a').style.background = '#fafafa'
+	})
+}
+document.querySelector('#butAbout').addEventListener('click', function()
+	{
+		scrollTo(event,'.about')
+	})
+document.querySelector('#butOur').addEventListener('click', function()
+	{
+		scrollTo(event,'.pets')
+	})
+document.querySelector('#butHelp').addEventListener('click', function()
+	{
+		scrollTo(event,'.help')
+	})
+document.querySelector('#butCont').addEventListener('click', function()
+	{
+		scrollTo(event,'.footer')
+	})
+// document.addEventListener('mousemove', parallax)
